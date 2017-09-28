7ba602872870e05d7632b457efc6735b9fe676bd
pvDung <dungvatoi12@gmail.com>
2017-09-28 23:13:54 +0700

    style form and add destoy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     def index
         @articles = Article.all
     end
@@ -20,15 +20,12 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -37,7 +34,17 @@ class ArticlesController < ApplicationController
         end
     end
 
+
+    def destroy
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted"
+        redirect_to articles_path
+    end
+
     private
+        def set_article
+            @article = Article.find(params[:id])
+        end
         def article_params
             params.require(:article).permit(:title, :description) 
         end
