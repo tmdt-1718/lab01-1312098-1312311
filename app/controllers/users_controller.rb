82a444c51ef3b9e04807f70b29ad7f1fb3025efd
pvDung <dungvatoi12@gmail.com>
2017-09-30 01:13:52 +0700

    Fix login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the Travel Blog #{@user.username}"
             redirect_to articles_path
         else 
