ea88321810c791a0cc79269eb81400b95bc85faa
pvDung <dungvatoi12@gmail.com>
2017-09-29 18:39:02 +0700

    Add signup and edit users page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,9 @@ class ArticlesController < ApplicationController
 
     def create
         #render plain: params[:article].inspect
+        debugger
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:success] = "Article was successfully create"
             redirect_to article_path(@article)            
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get 'welcome/about', to: 'welcome#about'
 
   resources :articles
+
+  get 'signup', to: "users#new"
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
