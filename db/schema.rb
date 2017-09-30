586bdd219077cf17737f14e4c790d9fdc85c649d
pvDung <dungvatoi12@gmail.com>
2017-09-30 12:57:54 +0700

    update all view with category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930034739) do
+ActiveRecord::Schema.define(version: 20170930051206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
