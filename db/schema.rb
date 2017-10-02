6b2cac443b965e992bba1fbb00ac0c6a1dc54158
pvDung <dungvatoi12@gmail.com>
2017-10-02 16:52:42 +0700

    Add gem omniauth-facebook and create omniauth.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001044151) do
+ActiveRecord::Schema.define(version: 20171002093534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20171001044151) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "provider"
+    t.string "uid"
   end
 
 end
