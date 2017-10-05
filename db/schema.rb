8bfbedd1c5ef9daff590c57800d820c1070260de
Huỳnh Văn Lâm <32082503+1312311@users.noreply.github.com>
2017-10-06 01:07:30 +0700

    Add carrierwave and cloudinary gem. Add avatar for user and upload avatar to cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003110519) do
+ActiveRecord::Schema.define(version: 20171005163953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -43,6 +49,12 @@ ActiveRecord::Schema.define(version: 20171003110519) do
     t.datetime "updated_at"
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.string "name"
+    t.integer "album_id"
+    t.string "image"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -52,6 +64,7 @@ ActiveRecord::Schema.define(version: 20171003110519) do
     t.boolean "admin", default: false
     t.string "provider"
     t.string "uid"
+    t.string "avatar"
   end
 
 end
