3d006a5202ded28dd24c8a96fcecdff068ba29d4
Huỳnh Văn Lâm <32082503+1312311@users.noreply.github.com>
2017-10-06 13:10:55 +0700

    Add photos model and view photo to album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,8 @@
 class Album < ApplicationRecord
     belongs_to :user
 
+    has_many :photos
+
     validates :name, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
     validates :user_id, presence: true
